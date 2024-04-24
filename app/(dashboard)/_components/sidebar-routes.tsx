c77b06c07289d60551aeb14fa0e58cc47247777f
taufik <taufika.ug20.cse@nitp.ac.in>
2024-04-24 21:11:24 +0530

    upto adding createcourse and added toaster

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,8 +1,9 @@
 
 "use clients";
 
-import {Layout,Compass} from "lucide-react"
+import {Layout,Compass, List, BarChart} from "lucide-react"
 import { SidebarItem } from "./sidebar-items";
+import { usePathname } from "next/navigation";
 const guestRoutes=[{
  icon: Layout, 
  label: "Dashboard",
@@ -15,8 +16,23 @@ const guestRoutes=[{
 
 }
 ]
+const teacherRoutes =[
+    {
+        icon: List, 
+        label: "Courses",
+        href:"/teacher/courses",
+       },
+       {
+         icon: BarChart,
+         label: "Analytics",
+         href: "/teacher/analytics"
+       
+       }
+]
 export const SidebarRoutes = () => {
-   const routes= guestRoutes;
+    const pathname= usePathname();
+    const isTeacherPage= pathname?.includes("/teacher")
+    const routes= isTeacherPage?teacherRoutes:guestRoutes;
     return ( 
         
         <div className=" flex-col w-full">
